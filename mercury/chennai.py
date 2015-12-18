5c947768b6db8433018d55e2e38c3ea81709a86e
Sivasubramanyam A <sivasubramanyama@gmail.com>
2015-12-18 14:10:58 +0530

    Initial commit

---
+++ mercury/chennai.py
@@ -0,0 +1,20 @@
+import feedparser
+import facebook
+import re
+from time import strftime
+
+feed = "http://spotthestation.nasa.gov/sightings/xml_files/India_None_Chennai.xml"
+graph = facebook.GraphAPI(access_token='')
+
+parsed = feedparser.parse(feed)
+if parsed.entries:
+	next_pass = parsed.entries[0]
+	pass_title = next_pass.title
+	date_today = strftime("%Y-%m-%d")
+	if date_today not in pass_title:
+		pass_details = next_pass.description
+		pass_details = pass_details.replace('<br />', '')
+		pass_details = pass_details.encode('utf-8')
+		post = "ISS Pass for today!\n" + pass_details
+		graph.put_object(parent_object='me', connection_name='feed', 
+			message=post)
