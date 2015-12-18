1542b2a344c3c2733640dfba43c5ab204c922efc
Sivasubramanyam A <sivasubramanyama@gmail.com>
2015-12-18 14:11:17 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='mercury',
+    version='0.1',
+    description='Post ISS Pass details to the CAC page',
+    license='MIT',
+    scripts=[],
+    install_requires=[
+    	'facebook-sdk',
+    ],
+    zip_safe=False
+)
