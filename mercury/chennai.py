5187ea7b66a4889e5ed408ce795cc3bc5f7c581c
Sivasubramanyam A <sivasubramanyama@gmail.com>
2016-01-21 17:22:48 +0530

    Make script fetch details for the next day to post morning passes beforehand

--- mercury/chennai.py
+++ mercury/chennai.py
@@ -1,7 +1,7 @@
 import feedparser
 import facebook
 import re
-from time import strftime
+from datetime import date, timedelta
 
 with open('mercury/mercury/access_token', 'r') as file:
 	access_token = file.read()
@@ -14,10 +14,11 @@ parsed = feedparser.parse(feed)
 if parsed.entries:
 	for entry in parsed.entries:
 		pass_title = entry.title
-		date_today = strftime("%Y-%m-%d")
-		if date_today in pass_title:
+		date_tomorrow = date.today() + timedelta(days=1)
+		date_tomorrow_string = date_tomorrow.strftime("%Y-%m-%d")
+		if date_tomorrow_string in pass_title:
 			pass_details = entry.description
 			pass_details = pass_details.replace('<br />', '')
 			pass_details = pass_details.encode('utf-8')
-			post = "ISS Pass for today!\n" + pass_details
+			post = "Upcoming ISS Pass!\n" + pass_details
 			graph.put_object(parent_object='me', connection_name='feed', message=post)
