f0225f106a86894056eb168d7606fba1675bfb60
Sivasubramanyam A <sivasubramanyama@gmail.com>
2016-01-21 16:50:34 +0530

    Add provision for multiple passes in a single day

--- mercury/chennai.py
+++ mercury/chennai.py
@@ -12,13 +12,12 @@ feed = "http://spotthestation.nasa.gov/sightings/xml_files/India_None_Chennai.xm
 parsed = feedparser.parse(feed)
 
 if parsed.entries:
-	next_pass = parsed.entries[0]
-	pass_title = next_pass.title
-	date_today = strftime("%Y-%m-%d")
-	if date_today in pass_title:
-		pass_details = next_pass.description
-		pass_details = pass_details.replace('<br />', '')
-		pass_details = pass_details.encode('utf-8')
-		post = "ISS Pass for today!\n" + pass_details
-		graph.put_object(parent_object='me', connection_name='feed', 
-			message=post)
+	for entry in parsed.entries:
+		pass_title = entry.title
+		date_today = strftime("%Y-%m-%d")
+		if date_today in pass_title:
+			pass_details = entry.description
+			pass_details = pass_details.replace('<br />', '')
+			pass_details = pass_details.encode('utf-8')
+			post = "ISS Pass for today!\n" + pass_details
+			graph.put_object(parent_object='me', connection_name='feed', message=post)
